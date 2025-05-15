efd8ca03214c33c33c088f19d8f7abe606ba1946
Tazz <tazz@tazz.cloud>
2025-05-14 20:54:15 -0400

    update sha for cel

--- ports/cel/portfile.cmake
+++ ports/cel/portfile.cmake
@@ -2,7 +2,8 @@ vcpkg_from_github(
   OUT_SOURCE_PATH SOURCE_PATH
   REPO google/cel-cpp
   REF v0.11.0
-  SHA512 0
+  SHA512
+    5b16be211a6d8280e5a4dc89c4736a33a282b6b9c84f28551a192b037ef085695c2d8ae45fbd7ab22255ce50f9dcb8d99b07f04f775cb2b0b5e88b4d621c8b43
   HEAD_REF master)
 vcpkg_cmake_configure(SOURCE_PATH ${SOURCE_PATH})
 vcpkg_cmake_install()
